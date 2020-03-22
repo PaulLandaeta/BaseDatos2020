f1ce369fe5065129910ee9cd843a363b5ca6b579
Paul Wilker Landaeta Flores <paulwilkerlf@mojix.com>
2020-03-22 13:08:11 -0400

    fix main class

--- clase 6/Main.java
+++ clase 6/Main.java
@@ -1,84 +1,23 @@
 package restaurant;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class Conector {
-	/** 
-	 * Atributos de clase 
-	 */
-	private static Connection con;
-	private static Conector INSTANCE = null;
-	
-	/**
-	 * Constructor
-	 */
-	private Conector(){ 
-		
-	}
-	
-	/**
-	 * Crear instancia 
-	 */
-	
-	private synchronized static void crearInstancia() {
-		if(INSTANCE == null) {
-			INSTANCE = new Conector();
-			crearConexion();
-		}
-	}
-	
-	/**
-	 * Obtener instancia
-	 */
-	
-	public static Conector getInstancia() {
-		if (INSTANCE == null ) {
-			crearInstancia(); 
-		}
-		return INSTANCE;	
-	}
+public class Main {
 
-	/** 
-	 * Crear Conexion 
-	 */
-	private static void crearConexion() {
-		String host = "127.0.0.1";
-		String user = "pepito";
-		String password = "control123!";//Poner su password
-		String dataBase = "cuarentena";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Probando");
+		Conector instancia = Conector.getInstancia();
 		try {
-			// Importando la libreria de conexion de mysql
-			Class.forName("com.mysql.jdbc.Driver");
-			// Url de conexion
-			String urlConexion = "jdbc:mysql://"+host+"/"+dataBase+"?user="+user+"&password="+password;
-			con = DriverManager.getConnection(urlConexion);
-			System.out.println("Lo lograste :') ");
-			
-		}catch (Exception e) {
-			System.out.println("Error al conectar a la base de datos");
+			ArrayList<String> listCompanies = instancia.getVendors();
+			for(String company:listCompanies) {
+				System.out.println(company);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 	
-	public ArrayList<String> getVendors() throws SQLException {
-		ArrayList<String> listaVendors = new ArrayList<String>(); 
-		PreparedStatement ps = con.prepareStatement("Select * from vendors");
-		ResultSet rs = ps.executeQuery();
-		while(rs.next()) {
-			listaVendors.add(rs.getString("companyname"));
-		}
-		rs.close();
-		return listaVendors;
-	}
-	
-	/* 
-	 * 1) Crear un nuevo usuario en Mysql
-	 * 2) Conectarse a la base cuarentena con el nuevo usuario 
-	 * 3) Mostrar los nombre de los pacientes
-	 * 4) Mostrar el consultorio y nombre de los doctores
-	 */
+
 }
